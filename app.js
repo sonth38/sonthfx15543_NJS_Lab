b82369a8a3668d115a2d129f942c58dc5ae54fa3
Son Ta <sontavac@gmail.com>
2022-05-23 13:56:45 +0700

    Lab1.4

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method,req.headers);
-
+    // console.log(req.url, req.method,req.headers);
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+      }
     /* Thoát khỏi server */
     // process.exit(); 
 
     // Cách cài đặt response trên máy chủ trả về dữ liệu có dạng html
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<http>')
-    res.write('<header><title>My First Page</title></header>')
-    res.write('<body><h1>Hello from my Node.js server!</h1></body>')
-    res.write('</http>')
-    // res.write('<h1>THis is a test</h1>')
-    res.end()
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<http>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js server!</h1></body>');
+    res.write('</http>');
+    res.end();
 });
 
 server.listen(3000) /* khởi chạy server port 3000 hoặc port bất kì */
