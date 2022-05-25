773fa8c904649efb9dac672dbc55874cec1b2573
Son Ta <sontavac@gmail.com>
2022-05-25 23:32:45 +0700

    Lab2.5

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const http = require('http');
 
 const express = require('express')
 
+const bodyParser = require('body-parser')
+
 const app = express()
 
-app.use((req, res, next)=>{
-    console.log('In the middleware')
-    next()
-})
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/add-product',(req, res, next)=>{
-    console.log('In the another middleware 1')
-    res.send('<h1>The Add product page</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+})
+
+app.use('/product',(req, res)=>{
+    console.log(req.body)
+    res.redirect('/')
 })
 
 app.use('/',(req, res, next)=>{
-    console.log('In the another middleware 2')
+
     res.send('<h1>Hello from Express.js</h1>')
 })
 
