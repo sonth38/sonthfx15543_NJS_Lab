7a4b3c63a48bd5bb39e39fa497622611b51f431f
Son Ta <sontavac@gmail.com>
2022-05-25 22:36:21 +0700

    Lab2.2

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next)=>{
+    console.log('In the middleware')
+    next()
+})
+
+app.use((req, res, next)=>{
+    console.log('In the another middleware')
+})
+
 const server = http.createServer(app);
 
 server.listen(3000) /* khởi chạy server port 3000 hoặc port bất kì */
