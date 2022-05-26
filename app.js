0b4bac23d8b7cf6c6bbe1bf10e3b82288cd953b7
Son Ta <sontavac@gmail.com>
2022-05-26 10:59:16 +0700

    Lab2.12

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const http = require('http');
 
 const express = require('express')
@@ -17,7 +19,7 @@ app.use('/admin',adminRouter)
 app.use(shopRouter)
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 
