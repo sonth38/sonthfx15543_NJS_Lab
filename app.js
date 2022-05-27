777ff877b38fd35fd2fc5ac1d1df8419743e9367
Son Ta <sontavac@gmail.com>
2022-05-27 17:02:37 +0700

    Lab3.10

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/admin',adminData.routes)
 app.use(shopRouter)
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'})
+    res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''})
 })
 
 app.listen(3000);
