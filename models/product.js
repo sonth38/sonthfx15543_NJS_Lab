2aa4a2b7860cc3dfb1404f6547e20eaf94334f35
Son Ta <sontavac@gmail.com>
2022-05-29 11:05:45 +0700

    Lab4.6

--- models/product.js
+++ models/product.js
@@ -1,23 +1,30 @@
 const fs = require('fs')
 const path = require('path')
 
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'product.json'
+)
+
+const getProductFromFile = (cb) => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return cb([])
+        }
+        else {
+            return cb(JSON.parse(fileContent))
+        }
+    })
+}
+
 module.exports = class Product {
     constructor(t) {
         this.title = t
     }
 
     save() {
-        const p = path.join(
-            path.dirname(require.main.filename),
-            'data',
-            'product.json'
-        )
-
-        fs.readFile(p, (err, fileContent) => {
-            let products = []
-            if (!err) {
-                products = JSON.parse(fileContent)
-            }
+        getProductFromFile(products => {
             products.push(this)
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err)
@@ -26,17 +33,6 @@ module.exports = class Product {
     }
 
     static fetchAll(cb) {
-        const p = path.join(
-            path.dirname(require.main.filename),
-            'data',
-            'product.json'
-        )
-
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                cb([])
-            }
-            cb(JSON.parse(fileContent))
-        })
+        getProductFromFile(cb)
     }
 }
