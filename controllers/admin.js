291bf42805e7ad47aa167b3f84b77395582d95ab
Son Ta <sontavac@gmail.com>
2022-05-31 09:09:00 +0700

    Lab4.16

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -37,6 +37,17 @@ exports.getEditProduct = (req, res, next) => {
     });
   })
 };
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId
+  const updateTitle = req.body.title
+  const updateImageUrl = req.body.imageUrl
+  const updatePrice = req.body.price
+  const updateDesc = req.body.description
+  const updateProduct = new Product(prodId, updateTitle, updateImageUrl, updateDesc, updatePrice)
+  updateProduct.save()
+  res.redirect('/admin/products')
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
