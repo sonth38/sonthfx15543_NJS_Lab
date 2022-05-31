0a490c6967b2835651ec001e4a3a25c3110a4eeb
Son Ta <sontavac@gmail.com>
2022-05-31 23:20:14 +0700

    Lab5.6

--- models/product.js
+++ models/product.js
@@ -11,9 +11,19 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-
+  constructor (id, title, imageUrl, description, price) {
+    this.id = id,
+    this.title = title,
+    this.imageUrl = imageUrl,
+    this.description = description,
+    this.price = price
+  }
 
   save() {
+    return db.execute(
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+       [this.title, this.price, this.imageUrl, this.description]
+    )
   }
 
   static deleteById(id) {
