8449e12e0e36568b388dd075e8bac24823e4b817
Son Ta <sontavac@gmail.com>
2022-06-01 11:14:26 +0700

    Lab5.11

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
-const Sequelize = require('sequelize')
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'root', '3893', {
-    dialect: 'mysql',
-    host: 'localhost'
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = sequelize
+module.exports = sequelize;
