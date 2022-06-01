3d2ac98b926c6341d689c4397c276cb8f37367d7
Son Ta <sontavac@gmail.com>
2022-06-01 14:15:29 +0700

    Lab5.14

--- controllers/admin.js
+++ controllers/admin.js
@@ -63,11 +63,14 @@ exports.postDeleteProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  });
+  Product.findAll()
+    .then(product => {
+      res.render('admin/products', {
+        prods: product,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
+
 };
