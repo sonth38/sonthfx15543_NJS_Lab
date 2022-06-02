e239703c6c30d1585fbb59a2d85e323e1e2b4a06
Son Ta <sontavac@gmail.com>
2022-06-02 13:55:57 +0700

    Lab5.27

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,3 @@
-const Cart = require("../models/cart");
 const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
@@ -139,6 +138,31 @@ exports.getOrders = (req, res, next) => {
     path: "/orders",
     pageTitle: "Your Orders",
   });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          return order.addProducts(
+            products.map((product) => {
+              product.orderItem = { quantity: product.cartItem.quantity };
+              return product;
+            })
+          );
+        })
+        .catch((err) => console.log(err));
+    })
+    .then(result => {
+      res.redirect('/orders')
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
--- models/order.js
+++ models/order.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../util/database')
 
-const Order = sequelize.define('oder', {
+const Order = sequelize.define('order', {
     id : {
         type : Sequelize.INTEGER,
         allowNull: false,
