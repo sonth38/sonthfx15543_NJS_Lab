ab7668a971ef7c8f5b5253086bdf34a0b9291a28
Son Ta <sontavac@gmail.com>
2022-06-02 11:05:20 +0700

    Lab5.26

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const Product = require('./models/product')
 const User = require('./models/user')
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
+
 
 const app = express();
 
@@ -42,10 +45,14 @@ User.hasOne(Cart)
 Cart.belongsTo(User)
 Cart.belongsToMany(Product, {through: CartItem})
 Product.belongsToMany(Cart, {through: CartItem})
+Order.belongsTo(User)
+User.hasMany(Order)
+Order.belongsToMany(Product, {through: OrderItem})
+
 
 sequelize
-    // .sync({force: true})
-    .sync()
+    .sync({force: true})
+    // .sync()
     .then(result => {
         return User.findByPk(1)
         // console.log(result)
