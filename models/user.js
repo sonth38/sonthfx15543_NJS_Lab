d899f4cfedda14d1c4303eb1b947e6eb193c6cd0
Son Ta <sontavac@gmail.com>
2022-06-03 17:04:04 +0700

    Lab6.16

--- models/user.js
+++ models/user.js
@@ -2,8 +2,11 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class User {
-  constructor(userName, email) {
-    (this.name = userName), (this.email = email);
+  constructor(userName, email, cart, id) {
+    (this.name = userName),
+      (this.email = email),
+      (this.cart = cart),
+      (this._id = id);
   }
 
   save() {
@@ -13,6 +16,15 @@ class User {
       .insertOne(this)
       .then(result => console.log("User", result))
       .catch(err => console.log(err));
+  }
+
+  addToCart(product) {
+    const updateCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    db.collection("user").updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      {$set: {card: updateCart,}}
+    );
   }
 
   static findById(userId) {
