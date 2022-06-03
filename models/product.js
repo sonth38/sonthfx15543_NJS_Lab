e9924624afdd17c7ce1c17ab8ff92d4e2b1dbea0
Son Ta <sontavac@gmail.com>
2022-06-03 10:07:07 +0700

    Lab6.3

--- models/product.js
+++ models/product.js
@@ -1,3 +1,5 @@
+const getDb = require('../util/database').getDb
+
 class Product {
   constructor(title, price, description, imageUrl) {
     this.title = title, 
--- util/database.js
+++ util/database.js
@@ -2,13 +2,29 @@ const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
-const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://root:3893@cluster0.oiywn.mongodb.net/?retryWrites=true&w=majority')
-    .then(client => {
-      console.log('Connect!')
-      callback(client)
+let _db;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    "mongodb+srv://root:3893@cluster0.oiywn.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connect!");
+      _db = client.db()
+      callback();
     })
-    .catch((err) => console.log(err));
+    .catch(err => {
+      console.log(err);
+      throw(err);
+    });
 };
 
-module.exports = mongoConnect
+const getDb = () => {
+  if (_db) {
+    return _db
+  }
+  throw 'No database found!'
+} 
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
