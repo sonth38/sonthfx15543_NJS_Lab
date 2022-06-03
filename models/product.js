3a650ec33793b2dc2e8a218496954ad32452a875
Son Ta <sontavac@gmail.com>
2022-06-03 11:01:38 +0700

    Lab6.6

--- models/product.js
+++ models/product.js
@@ -3,16 +3,30 @@ const getDb = require("../util/database").getDb;
 class Product {
   constructor(title, price, description, imageUrl) {
     (this.title = title),
-      (this.price = price),
-      (this.description = description),
-      (this.imageUrl = imageUrl);
+    (this.price = price),
+    (this.description = description),
+    (this.imageUrl = imageUrl);
   }
 
   save() {
     const db = getDb();
-    return db.collection("products")
+    return db
+      .collection("products")
       .insertOne(this)
-      .then(result => console.log('save Product',result))
+      .then(result => console.log("save Product", result))
+      .catch(err => console.log(err));
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then(product => {
+        console.log('fetchAll Product', product)
+        return product
+      })
       .catch(err => console.log(err));
   }
 }
