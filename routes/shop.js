7c84667240a390d45240b77578a8d34e2302c5b9
Son Ta <sontavac@gmail.com>
2022-06-04 22:51:30 +0700

    Lab6.21

--- routes/shop.js
+++ routes/shop.js
@@ -16,7 +16,7 @@ router.get('/cart', shopController.getCart);
 
 router.post('/cart', shopController.postCart);
 
-// router.post('/cart-deletet-item', shopController.postCartDeleteProduct);
+router.post('/cart-deletet-item', shopController.postCartDeleteProduct);
 
 // router.get('/orders', shopController.getOrders);
 
