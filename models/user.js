155f33b5f5c86293cd5b55e6a7a39502e8c6d9b6
Son Ta <sontavac@gmail.com>
2022-06-04 21:04:39 +0700

    Lab6.19

--- models/user.js
+++ models/user.js
@@ -3,41 +3,64 @@ const getDb = require("../util/database").getDb;
 
 class User {
   constructor(username, email, cart, id) {
-    this.name = username,
-    this.email = email,
-    this.cart = cart,
-    this._id = id;
+    (this.name = username),
+      (this.email = email),
+      (this.cart = cart),
+      (this._id = id);
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("users")
-      .insertOne(this)
+    return db.collection("users").insertOne(this);
   }
 
   addToCart(product) {
     const cartProductIndex = this.cart.items.findIndex(cp => {
       return cp.productId.toString() === product._id.toString();
-    })
-    let newQuantity = 1
-    const updateCartItems = [...this.cart.items] 
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+
     if (cartProductIndex >= 0) {
       newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-      updateCartItems[cartProductIndex].quantity = newQuantity;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
     } else {
-      updateCartItems.push({
+      updatedCartItems.push({
         productId: new mongodb.ObjectId(product._id),
         quantity: newQuantity,
       });
     }
-
-    const updateCart = { items: updateCartItems };
+    const updatedCart = {
+      items: updatedCartItems,
+    };
     const db = getDb();
-    return db.collection("users").updateOne(
-      { _id: new mongodb.ObjectId(this._id) },
-      { $set: { cart: updateCart } }
-    );
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then(product => {
+        return product.map(p => {
+          return {
+            ...p,
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity,
+          };
+        });
+      });
   }
 
   static findById(userId) {
