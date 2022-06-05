d164ec29505680493d79a0cad8794be5d30d0840
Son Ta <sontavac@gmail.com>
2022-06-05 22:57:56 +0700

    Lab7.1

--- app.js
+++ app.js
@@ -3,36 +3,45 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
+// const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 const User = require("./models/user");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('6299d1e566977b92f45145ed')
-      .then(user => {
-          req.user = new User(user.name, user.email, user.cart, user._id)
-          next()
-      })
-      .catch(err => console.log(err))
+  User.findById("6299d1e566977b92f45145ed")
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-  console.log(client);
-  app.listen(3000);
-});
+// mongoConnect(client => {
+//   console.log(client);
+//   app.listen(3000);
+// });
+
+mongoose
+  .connect(
+    "mongodb+srv://root:3893@cluster0.oiywn.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
--- routes/shop.js
+++ routes/shop.js
@@ -6,22 +6,22 @@ const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', shopController.getIndex);
+// router.get('/', shopController.getIndex);
 
-router.get('/products', shopController.getProducts);
+// router.get('/products', shopController.getProducts);
 
-router.get('/products/:productId', shopController.getProduct)
+// router.get('/products/:productId', shopController.getProduct)
 
-router.get('/cart', shopController.getCart);
+// router.get('/cart', shopController.getCart);
 
-router.post('/cart', shopController.postCart);
+// router.post('/cart', shopController.postCart);
 
-router.post('/cart-deletet-item', shopController.postCartDeleteProduct);
+// router.post('/cart-deletet-item', shopController.postCartDeleteProduct);
 
-router.get('/orders', shopController.getOrders);
+// router.get('/orders', shopController.getOrders);
 
-router.post('/create-order', shopController.postOrder);
+// router.post('/create-order', shopController.postOrder);
 
-// router.get('/checkout', shopController.getCheckout);
+// // router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
