9281c646e95a33045fddce5de00c2731e66e0926
Son Ta <sontavac@gmail.com>
2022-06-19 16:02:21 +0700

    Lab8.1

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+exports.getLogin = (req, res, next) => {
+    res.render("auth/login", {
+        pageTitle: "Login",
+        path: "/login",
+        isAuthenticated: req.isLoggedIn
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    req.isLoggedIn = true
+    res.redirect('/')
+};
--- controllers/shop.js
+++ controllers/shop.js
@@ -9,6 +9,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "All Products",
         path: "/products",
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -22,6 +23,7 @@ exports.getProduct = (req, res, next) => {
         product: product,
         pageTitle: product.title,
         path: "/products",
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -45,6 +47,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: "Shop",
         path: "/",
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -60,6 +63,7 @@ exports.getCart = (req, res, next) => {
         path: "/cart",
         pageTitle: "Your Cart",
         products: products,
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -98,6 +102,7 @@ exports.getOrders = (req, res, next) => {
         path: "/orders",
         pageTitle: "Your Orders",
         orders: orders,
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -133,5 +138,6 @@ exports.getCheckout = (req, res, next) => {
   res.render("shop/checkout", {
     path: "/checkout",
     pageTitle: "Checkout",
+    isAuthenticated: req.isLoggedIn
   });
 };
