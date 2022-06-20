eb164c18ccdfd8c4f6f8788693fe7ac8f94fb150
Son Ta <sontavac@gmail.com>
2022-06-20 11:17:45 +0700

    Lab8.7

--- app.js
+++ app.js
@@ -4,29 +4,38 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 // const mongoConnect = require("./util/database").mongoConnect;
-
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://root:3893@cluster0.oiywn.mongodb.net/shop?retryWrites=true&w=majority';
+
+const app = express();
+
+// Thiết lập lưu trữ session trong mongoDb
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Sử dụng middleware session
 app.use(
-  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+  session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store })
 );
 
 app.use((req, res, next) => {
@@ -50,9 +59,7 @@ app.use(errorController.get404);
 // });
 
 mongoose
-  .connect(
-    'mongodb+srv://root:3893@cluster0.oiywn.mongodb.net/shop?retryWrites=true&w=majority'
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
