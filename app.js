cd841a223be98f6b0e4708be9d6a0c1369cdec0c
Son Ta <sontavac@gmail.com>
2022-06-20 16:19:01 +0700

    Lab8.12

--- app.js
+++ app.js
@@ -35,20 +35,25 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Sử dụng middleware session
 app.use(
-  session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store })
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 
 // Sử dụng 1 middleware user để gửi req.user sang middleware phía sau
 app.use((req, res, next) => {
-	if (!req.session.user) {
-		return next()
-	}
-	User.findById(req.session.user._id)
-		.then(user => {
-			req.user = user;
-			next();
-		})
-		.catch(err => console.log(err));
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -65,18 +70,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Son',
-          email: 'sontahvac@gmail.com',
-          cart: {
-            items: [],
-          },
-        });
-      }
-      user.save();
-    });
     app.listen(3000);
   })
   .catch(err => console.log(err));
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,59 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-    res.render('auth/login', {
-        pageTitle: 'Login',
-        path: '/login',
-        isAuthenticated: false,
-    });
+  res.render('auth/login', {
+    pageTitle: 'Login',
+    path: '/login',
+    isAuthenticated: false,
+  });
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById('629d6a86a9dfd36036ec046e')
-        .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            req.session.save(err => {
-				console.log(err)
-				res.redirect('/')
-            })
-        })
-        .catch(err => console.log(err));
+  User.findById('629d6a86a9dfd36036ec046e')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
-	req.session.destroy(err => {
-		console.log(err);
-		res.redirect('/');
-	});
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    pageTitle: 'Signup',
+    path: '/signup',
+    isAuthenticated: false,
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+        if (userDoc) {
+            return res.redirect('/signup')
+        }
+        const user = new User({
+            email: email,
+            password: password,
+            cart: { item: [] }
+        })
+        return user.save()
+    })
+    .then(result => {
+        res.redirect('/login')
+    })
+    .catch(err => console.log(err));
 };
