c3d53930d019b162ce2bce91570fd4c97d52226a
Son Ta <sontavac@gmail.com>
2022-06-20 10:14:39 +0700

    Lab8.5

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const mongoose = require("mongoose");
+const session = require('express-session');
 
-const errorController = require("./controllers/error");
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
 // const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
-const User = require("./models/user");
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Sử dụng middleware session
+app.use(
+  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
-  User.findById("629d6a86a9dfd36036ec046e")
+  User.findById('629d6a86a9dfd36036ec046e')
     .then(user => {
       req.user = user;
       next();
@@ -31,7 +38,7 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
@@ -44,7 +51,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://root:3893@cluster0.oiywn.mongodb.net/shop?retryWrites=true&w=majority"
+    'mongodb+srv://root:3893@cluster0.oiywn.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
     User.findOne().then(user => {
@@ -53,12 +60,12 @@ mongoose
           name: 'Son',
           email: 'sontahvac@gmail.com',
           cart: {
-            items: []
-          }
-        })
+            items: [],
+          },
+        });
       }
-      user.save()
-    })
-    app.listen(3000)
+      user.save();
+    });
+    app.listen(3000);
   })
   .catch(err => console.log(err));
