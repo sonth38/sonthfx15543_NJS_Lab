d40364cc0bf8e8ef2877224e9c8df88020280db2
Son Ta <sontavac@gmail.com>
2022-06-20 09:35:51 +0700

    Lab8.4

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 exports.getLogin = (req, res, next) => {
-    const isLoggedIn = req.get('Cookie').split(';')[0].trim('').split('=')[1] === 'true'
-    res.render("auth/login", {
-        pageTitle: "Login",
-        path: "/login",
-        isAuthenticated: isLoggedIn
-    });
+//   const isLoggedIn = req.get('Cookie').trim('').split('=')[1] === 'true';
+
+  res.render('auth/login', {
+    pageTitle: 'Login',
+    path: '/login',
+    isAuthenticated: false,
+  });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-cookie', 'loggedIn=true')
-    res.redirect('/')
+  res.setHeader('Set-cookie', 'loggedIn=true; Max-Age= 10');
+  res.redirect('/');
 };
