c9e6a36638924492d616624b9808e696ba41d112
Son Ta <sontavac@gmail.com>
2022-06-23 23:33:09 +0700

    Lab9.7

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,6 +32,16 @@ exports.getLogin = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    return res.status(442).render('/auth/login',{
+      pageTitle: 'Login',
+      path: '/login',
+      errorMessage: errors.array()[0].msg,
+    })
+  }
+
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
@@ -73,6 +83,11 @@ exports.getSignup = (req, res, next) => {
     path: '/signup',
     isAuthenticated: false,
     errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
   });
 };
 
@@ -88,6 +103,11 @@ exports.postSignup = (req, res, next) => {
       pageTitle: 'Signup',
       path: '/signup',
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword
+      }
     });
   }
 
