85af6fb3e85c7f29c43ea3dabea755d18122cc9a
Son Ta <sontavac@gmail.com>
2022-06-23 10:53:55 +0700

    Lab9.4

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -12,17 +12,25 @@ router.post('/logout', authController.postLogout);
 
 router.get('/signup', authController.getSignup);
 router.post(
-'/signup',
-check('email')
-	.isEmail()
-	.withMessage('Please enter a valid email')
-	.custom((value, { req }) => {
-		if (value === 'test@test.com') {
-			throw new Error('This email address if forbidden');
-		}
-		return true;
-	}),
-authController.postSignup
+  '/signup',
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com') {
+          throw new Error('This email address if forbidden');
+        }
+        return true;
+      }),
+    body(
+      'password',
+      'Please enter a password with only number and text and at least 5 characters'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.postSignup
 );
 
 router.get('/reset', authController.getReset);
