6e33286e2c2e8cdc0332c26da7951f7f3b5539c8
Son Ta <sontavac@gmail.com>
2022-06-25 10:50:31 +0700

    Lab10.3

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const multer = require('multer');
 
 // CSRF
 const csrf = require('csurf');
@@ -39,6 +38,8 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({dest:'image'}).single('image'))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Sử dụng middleware session
--- controllers/admin.js
+++ controllers/admin.js
@@ -16,10 +16,11 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.image;
+  const imageUrl = req.file;
   const price = req.body.price;
   const description = req.body.description;
 
+  console.log(imageUrl)
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
