3292f0d4deb89d46409d068c57c5234f5cff1123
Son Ta <sontavac@gmail.com>
2022-06-26 14:03:14 +0700

    Lab10.18

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,6 +6,8 @@ const Product = require('../models/product');
 const user = require('../models/user');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getProducts = (req, res, next) => {
   Product.find()
     .then(products => {
@@ -51,7 +53,11 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+  const page = req.query.page
+
   Product.find()
+    .skip((page - 1) * ITEMS_PER_PAGE)
+    .limit(ITEMS_PER_PAGE)
     .then(products => {
       res.render('shop/index', {
         prods: products,
